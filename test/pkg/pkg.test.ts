4a41c1b9ad6fec4d697c9ec569582a11a4294abe
Misha Kaletsky <mmkal@users.noreply.github.com>
2023-08-13 18:52:42 -0400

    actually don't have a /esm entrypoint

--- test/pkg/pkg.test.ts
+++ test/pkg/pkg.test.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line mmkal/import/no-extraneous-dependencies
 import {test, expect} from '@playwright/test'
 import {fetchomatic, retry} from 'fetchomatic'
-import {fetchomatic as fetchomatic_esmEntrypoint, retry as retry_esmEntrypoint} from 'fetchomatic/esm'
 import {createRequire} from 'module'
 import {createTestSuite} from '../suite.js'
 
@@ -14,10 +13,6 @@ test.describe(`require pkg`, async () => {
   // eslint-disable-next-line mmkal/@typescript-eslint/consistent-type-imports
   const cjs = require('fetchomatic') as typeof import('fetchomatic')
   createTestSuite({test, expect, fetch: fetch, fetchomatic: cjs.fetchomatic, retry: cjs.retry})
-})
-
-test.describe(`esm entrypoint pkg`, async () => {
-  createTestSuite({test, expect, fetch: fetch, fetchomatic: fetchomatic_esmEntrypoint, retry: retry_esmEntrypoint})
 })
 
 // eslint-disable-next-line mmkal/@typescript-eslint/consistent-type-imports
@@ -35,26 +30,6 @@ test.describe(`async import pkg`, async () => {
     retry: new Proxy({} as typeof retry, {
       get(target, prop) {
         return asyncModule.retry[prop as keyof typeof asyncModule.retry]
-      },
-    }),
-  })
-})
-
-// eslint-disable-next-line mmkal/@typescript-eslint/consistent-type-imports
-let asyncEsmModule: typeof import('fetchomatic/esm')
-test.beforeAll(async () => {
-  asyncEsmModule = await import('fetchomatic/esm')
-})
-
-test.describe(`async import esm pkg`, async () => {
-  createTestSuite({
-    test,
-    expect,
-    fetch,
-    fetchomatic: (...args) => asyncEsmModule.fetchomatic(...args),
-    retry: new Proxy({} as typeof retry, {
-      get(target, prop) {
-        return asyncEsmModule.retry[prop as keyof typeof asyncEsmModule.retry]
       },
     }),
   })
