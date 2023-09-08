256f586212c9bdec8b755a14e3f430442be3e0c6
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2023-09-08 18:10:21 +0000

    browseable artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: process.env.TEST_DIR || './test/node',
   fullyParallel: true,
+  reporter: 'html',
   webServer: {
     command: "pnpm run test-server",
     url: 'http://localhost:7001/health',
