89189d20d2f499d02ddc43f900ee990512101146
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2023-09-08 18:38:14 +0000

    re-use outputDir

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from '@playwright/test';
 import * as path from 'path'
 
 const testDir = process.env.TEST_DIR || './test/node'
+const outputDir = path.join('playwright-report', path.basename(testDir));
 
 export default defineConfig({
   testDir,
   fullyParallel: true,
-  outputDir: path.join('playwright-report', path.basename(testDir)),
-  reporter: 'html',
+  outputDir,
+  reporter: [['html', {outputFolder: outputDir}]],
   webServer: {
     command: "pnpm run test-server",
     url: 'http://localhost:7001/health',
