3050b262258ff644706cb3b8ab2a6ab7168d0648
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2023-09-08 14:55:21 -0400

    browseable artifact (#4)
    * browseable artifact
    
    * separate dirs?
    
    * re-use outputDir
    
    * rm bogus log

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from '@playwright/test';
+import * as path from 'path'
+
+const testDir = process.env.TEST_DIR || './test/node'
+const outputDir = path.join('playwright-report', path.basename(testDir));
 
 export default defineConfig({
-  testDir: process.env.TEST_DIR || './test/node',
+  testDir,
   fullyParallel: true,
+  outputDir,
+  reporter: [['html', {outputFolder: outputDir}]],
   webServer: {
     command: "pnpm run test-server",
     url: 'http://localhost:7001/health',
